e37b0276994ed04d8670795ffcf761b7c087fe33
adilsonZamp <adilsonzampierijunior@gmail.com>
2026-05-12 23:15:00 -0300

    envio de email configurado e funcionando

--- backend/app/controllers/new_account_controller.ts
+++ backend/app/controllers/new_account_controller.ts
@@ -2,13 +2,22 @@ import User from '#models/user'
 import { signupValidator } from '#validators/user'
 import type { HttpContext } from '@adonisjs/core/http'
 import UserTransformer from '#transformers/user_transformer'
+import mail from '@adonisjs/mail/services/main'
 
 export default class NewAccountController {
   async store({ request, serialize }: HttpContext) {
     const { nome, email, password, tipo, cidade, estado, rua, numero } = await request.validateUsing(signupValidator)
+    const template = '<!DOCTYPE html><html><body style="font-family: Arial, sans-serif; line-height: 1.5; color: #333;">    <h2>Bem-vindo, {{nome}}!</h2>    <p>Seu cadastro foi realizado com sucesso.</p>    <p>Agora você já pode acessar a plataforma com as seguintes credenciais:</p>    <ul>        <li><strong>Email:</strong> {{email}}</li>        <li><strong>Senha:</strong> {{password}}</li>    </ul>    <p>        Recomendamos que você altere sua senha no primeiro acesso por segurança.    </p><br>    <p>Seja bem-vindo!</p>    <p><strong>Equipe de suporte</strong></p></body></html>';
     
     const user = await User.create({ nome, email, password, tipo, cidade, estado, rua, numero })
     const token = await User.accessTokens.create(user)
+    const html = template.replace('{{nome}}', nome)
+        .replace('{{email}}', email)
+        .replace('{{password}}', password)
+
+    await mail.send((mensagem) => {
+      mensagem.to(email).subject('Boas vindas ao BANIF').html(html)
+    })
 
     return serialize({
       user: UserTransformer.transform(user),
